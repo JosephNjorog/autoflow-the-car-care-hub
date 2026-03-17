9a77f827ddc1e226b17f61fc7eefa756876cf246
JosephNjorog <njorojoe11173@gmail.com>
2026-03-17 13:16:09 +0300

    feat: add GuestBook component import to App for guest booking functionality

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import NotFound from "./pages/NotFound";
 
 // Public pages
 import LandingPage from "./pages/LandingPage";
+import GuestBook from "./pages/GuestBook";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
